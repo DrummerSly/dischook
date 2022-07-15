44429a980eec051008adb6fd498827d35ea77b64
drumly <73012508+DrummerSly@users.noreply.github.com>
2022-07-15 20:07:10 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿
+namespace dischook
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Label label1;
+            System.Windows.Forms.Label label2;
+            System.Windows.Forms.Label label3;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.urlTextBox = new System.Windows.Forms.TextBox();
+            this.messageTextBox = new System.Windows.Forms.TextBox();
+            this.sendButton = new System.Windows.Forms.Button();
+            this.wuTextBox = new System.Windows.Forms.TextBox();
+            this.BackgroundWorker = new System.ComponentModel.BackgroundWorker();
+            label1 = new System.Windows.Forms.Label();
+            label2 = new System.Windows.Forms.Label();
+            label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new System.Drawing.Point(9, 57);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(32, 13);
+            label1.TabIndex = 3;
+            label1.Text = "URL:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new System.Drawing.Point(9, 105);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(53, 13);
+            label2.TabIndex = 4;
+            label2.Text = "Message:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new System.Drawing.Point(9, 9);
+            label3.Name = "label3";
+            label3.Size = new System.Drawing.Size(108, 13);
+            label3.TabIndex = 5;
+            label3.Text = "Webhook Username:";
+            // 
+            // urlTextBox
+            // 
+            this.urlTextBox.Location = new System.Drawing.Point(12, 73);
+            this.urlTextBox.Name = "urlTextBox";
+            this.urlTextBox.ShortcutsEnabled = false;
+            this.urlTextBox.Size = new System.Drawing.Size(214, 20);
+            this.urlTextBox.TabIndex = 0;
+            // 
+            // messageTextBox
+            // 
+            this.messageTextBox.Location = new System.Drawing.Point(12, 121);
+            this.messageTextBox.Multiline = true;
+            this.messageTextBox.Name = "messageTextBox";
+            this.messageTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.messageTextBox.ShortcutsEnabled = false;
+            this.messageTextBox.Size = new System.Drawing.Size(214, 182);
+            this.messageTextBox.TabIndex = 1;
+            // 
+            // sendButton
+            // 
+            this.sendButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.sendButton.Enabled = false;
+            this.sendButton.Location = new System.Drawing.Point(12, 309);
+            this.sendButton.Name = "sendButton";
+            this.sendButton.Size = new System.Drawing.Size(214, 23);
+            this.sendButton.TabIndex = 2;
+            this.sendButton.Text = "Send";
+            this.sendButton.UseVisualStyleBackColor = true;
+            this.sendButton.Click += new System.EventHandler(this.sendButton_Click);
+            // 
+            // wuTextBox
+            // 
+            this.wuTextBox.Location = new System.Drawing.Point(12, 25);
+            this.wuTextBox.Name = "wuTextBox";
+            this.wuTextBox.ShortcutsEnabled = false;
+            this.wuTextBox.Size = new System.Drawing.Size(214, 20);
+            this.wuTextBox.TabIndex = 6;
+            // 
+            // BackgroundWorker
+            // 
+            this.BackgroundWorker.WorkerSupportsCancellation = true;
+            this.BackgroundWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.BackgroundWorker_DoWork);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(240, 345);
+            this.Controls.Add(this.wuTextBox);
+            this.Controls.Add(label3);
+            this.Controls.Add(label2);
+            this.Controls.Add(label1);
+            this.Controls.Add(this.sendButton);
+            this.Controls.Add(this.messageTextBox);
+            this.Controls.Add(this.urlTextBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DiscHook";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox urlTextBox;
+        private System.Windows.Forms.TextBox messageTextBox;
+        private System.Windows.Forms.Button sendButton;
+        private System.Windows.Forms.TextBox wuTextBox;
+        private System.ComponentModel.BackgroundWorker BackgroundWorker;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Net;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace dischook
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        WebClient webClient = new WebClient();
+
+        private void sendMessage(string url, string message, string username)
+        {
+            using (webClient)
+            {
+                webClient.UploadValues(url, new NameValueCollection()
+                {
+                    {
+                        "username",
+                        username
+                    },
+                    {
+                        "content",
+                        message
+                    }
+                });
+            }
+        }
+
+        private void sendButton_Click(object sender, EventArgs e)
+        {
+            sendMessage(urlTextBox.Text, messageTextBox.Text, wuTextBox.Text);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = false;
+            
+            BackgroundWorker.RunWorkerAsync();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            BackgroundWorker.CancelAsync();
+            BackgroundWorker.Dispose();
+            webClient.Dispose();
+        }
+
+        private void BackgroundWorker_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            while (true)
+            {
+                if (!BackgroundWorker.CancellationPending)
+                {
+                    if (!string.IsNullOrWhiteSpace(wuTextBox.Text) && !string.IsNullOrWhiteSpace(urlTextBox.Text) && !string.IsNullOrWhiteSpace(messageTextBox.Text))
+                    {
+                        if (!Uri.IsWellFormedUriString(urlTextBox.Text, UriKind.Absolute))
+                        {
+                            sendButton.Enabled = false;
+                        }
+                        else
+                        {
+                            if (urlTextBox.Text.Contains("discord") && urlTextBox.Text.Contains("api") && urlTextBox.Text.Contains("webhooks") == true)
+                            {
+                                sendButton.Enabled = true;
+                            }
+                            else
+                            {
+                                sendButton.Enabled = false;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        sendButton.Enabled = false;
+                    }
+                    Thread.Sleep(1);
+                }
+            }
+        }
+    }
+}
